b8e4cdee1bbf3395bb7a98ae45e6fa1f8f05cb3a
Max <max.heinzer397@gmx.ch>
2023-01-06 11:46:31 +0100

    updates

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,98 @@ fn main() {
 
     let language = unsafe { tree_sitter_query() };
     parser.set_language(language).unwrap();
-    let source_code = "(block)@test";
+    let source_code = "(block)@test
+(mod_item
+ name: (identifier)@namespace)
+";
 
     let tree = parser.parse(source_code, None).unwrap();
     let root_node = tree.root_node();
+    let mut output = String::new();
     for child in root_node.children(&mut tree.walk()) {
-        println!("{}", child.kind());
+        println!("kind:{}", child.kind());
         println!("text:{}", child.utf8_text(source_code.as_bytes()).unwrap());
+        if child.kind() == "field_definition" {
+            println!("got it");
+            output.push_str("\n  ")
+        }
+        if child.child_count() == 0 {
+            output.push_str(child.utf8_text(source_code.as_bytes()).unwrap());
+        }
         for child_child in child.children(&mut tree.walk()) {
-            println!("  {}", child_child.kind());
+            println!("  kind:{}", child_child.kind());
             println!(
                 "  text:{}",
                 child_child.utf8_text(source_code.as_bytes()).unwrap()
             );
+            println!("{}", child.kind() == "field_definition");
+            println!("==========");
+            if child.kind() == "field_definition" {
+                println!("got it");
+                output.push_str("\n  ")
+            }
+            if child_child.child_count() == 0 {
+                output.push_str(child_child.utf8_text(source_code.as_bytes()).unwrap());
+            }
             for child_child_child in child_child.children(&mut tree.walk()) {
-                println!("    {}", child_child_child.kind());
+                println!("    kind:{}", child_child_child.kind());
                 println!(
                     "    text:{}",
                     child_child_child.utf8_text(source_code.as_bytes()).unwrap()
                 );
+                if child.kind() == "field_definition" {
+                    println!("got it");
+                    output.push_str("\n  ")
+                }
+                if child_child_child.child_count() == 0 {
+                    output.push_str(child_child_child.utf8_text(source_code.as_bytes()).unwrap());
+                }
+                for child_child_child_child in child_child_child.children(&mut tree.walk()) {
+                    println!("      kind:{}", child_child_child_child.kind());
+                    println!(
+                        "      text:{}",
+                        child_child_child_child
+                            .utf8_text(source_code.as_bytes())
+                            .unwrap()
+                    );
+                    if child.kind() == "field_definition" {
+                        println!("got it");
+                        output.push_str("\n  ")
+                    }
+                    if child_child_child_child.child_count() == 0 {
+                        output.push_str(
+                            child_child_child_child
+                                .utf8_text(source_code.as_bytes())
+                                .unwrap(),
+                        );
+                    }
+                    for child_child_child_child_child in
+                        child_child_child_child.children(&mut tree.walk())
+                    {
+                        println!("        kind:{}", child_child_child_child_child.kind());
+                        println!(
+                            "        text:{}",
+                            child_child_child_child_child
+                                .utf8_text(source_code.as_bytes())
+                                .unwrap()
+                        );
+                        if child.kind() == "field_definition" {
+                            println!("got it");
+                            output.push_str("\n  ")
+                        }
+                        if child_child_child_child_child.child_count() == 0 {
+                            output.push_str(
+                                child_child_child_child_child
+                                    .utf8_text(source_code.as_bytes())
+                                    .unwrap(),
+                            );
+                        }
+                    }
+                }
             }
         }
+        output.push('\n');
+        output.push('\n');
     }
+    println!("{output}");
 }

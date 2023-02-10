a64ab8c2cd98d6089eeb47210e1bbfb977c1a6ba
Max <max.heinzer397@gmx.ch>
2023-02-10 22:24:01 +0100

    refactor(main): implement `new()` for `RecursiveFileIterator`

--- src/file_iterator.rs
+++ src/file_iterator.rs
@@ -1,18 +1,34 @@
 use std::fs::read_dir;
-use std::fs::DirEntry;
+use std::path::PathBuf;
 
 pub struct RecursiveFileIterator {
-    pub stack: Vec<DirEntry>,
+    pub stack: Vec<PathBuf>,
+}
+
+impl RecursiveFileIterator {
+    pub fn new(path: PathBuf) -> Self {
+        let mut stack = Vec::new();
+        if path.is_file() {
+            stack.push(path);
+        } else if path.is_dir() {
+            for subdir in read_dir(path).unwrap().flatten() {
+                stack.push(subdir.path());
+            }
+        }
+        Self { stack }
+    }
 }
 
 impl Iterator for RecursiveFileIterator {
-    type Item = DirEntry;
+    type Item = PathBuf;
     fn next(&mut self) -> Option<Self::Item> {
         let entry = self.stack.pop()?;
         if entry.metadata().ok()?.is_dir() {
-            for sub_entry in read_dir(entry.path()).ok()?.flatten() {
-                self.stack.push(sub_entry);
+            for sub_entry in read_dir(entry.clone()).ok()?.flatten() {
+                self.stack.push(sub_entry.path());
             }
+            return Some(entry);
+        } else if entry.is_file() {
             return Some(entry);
         }
         None
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use clap::Parser as ClapParser;
 use tree_sitter::Parser;
 
-use std::fs::read_dir;
 use std::path::Path;
 
 mod args;
@@ -21,22 +20,13 @@ fn main() {
     parser.set_language(tree_sitter_query::language()).unwrap();
 
     let path = Path::new(&args.file).to_owned();
-    if path.is_file() {
-        format_file(&path, parser, &args);
-    } else if path.is_dir() {
-        let mut stack = Vec::new();
-        for subdir in read_dir(path).unwrap().flatten() {
-            stack.push(subdir);
-        }
-        let rec_iterator = RecursiveFileIterator { stack };
-        for file in rec_iterator {
-            let path = file.path();
-            if let Some(extension) = path.extension() {
-                if extension == "scm" {
-                    let mut parser = Parser::new();
-                    parser.set_language(tree_sitter_query::language()).unwrap();
-                    format_file(path.as_path(), parser, &args)
-                }
+    let rec_iterator = RecursiveFileIterator::new(path);
+    for file in rec_iterator {
+        if let Some(extension) = file.extension() {
+            if extension == "scm" {
+                let mut parser = Parser::new();
+                parser.set_language(tree_sitter_query::language()).unwrap();
+                format_file(file.as_path(), parser, &args)
             }
         }
     }
